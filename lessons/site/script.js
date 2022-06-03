8975a386473b82bfe08080a3846614af86dee985
nastya rak <nastya_r.94@mail.ru>
2022-06-03 21:40:12 +0300

    1

--- lessons/site/script.js
+++ lessons/site/script.js
@@ -5,6 +5,7 @@ var cards = [
         description: "Nightshade Snowboard - Women's - 2019/2020",
         cost: "$ 398.93",
         oldcost: "$ 499.00",
+        hello: "helloooo",
         },
     {
         imgURL: "images/4item.png",
@@ -23,18 +24,22 @@ var cards = [
         }
 ]
 
-var div = document.querySelector('.snowboards');
+var div = document.querySelector('.container2');
 var htmlStr = '';
 
 for (var i=0; i<cards.length; i++) {
     htmlStr += `
-    <div>
-    <img class="itempicture" src="${cards[i].imgURL}">
-    </div>
-    <div class="brand">Niche:&nbsp;</div>
-    <div class="description">Nightshade Snowboard - Women's - 2019/2020</div>
-    <div class="cost">$ 398.93</div>
-    <div class="oldcost"><s>$ 499.00</s></div>
+    <div class="snowboards">
+        <div>
+        <img class="itempicture" src="${cards[i].imgURL}">
+        </div>
+        <div class="brand">Niche:&nbsp;</div>
+        <div class="description">Nightshade Snowboard - Women's - 2019/2020</div>
+        <div class="cost">$ 398.93</div>
+        <div class="oldcost"><s>$ 499.00</s></div>
+        <div class="stars"><img src="images/stars.png"></div>
+        <div>${cards[i].hello}</div>
+        </div>
     `
 }
 
